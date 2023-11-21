1568ca7bb000358458e0b23147b8f41f0903b6fd
1450375918@qq.com <1450375918@qq.com>
2023-11-21 16:45:05 +0800

    第二次提交

--- src/main/java/ldu/Testdemo.java
+++ src/main/java/ldu/Testdemo.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class Testdemo {
     public static void main(String[] args) {
+        System.out.println("111");
         SpringApplication.run(Testdemo.class,args);
 
 
